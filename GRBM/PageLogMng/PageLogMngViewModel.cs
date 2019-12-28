793a180da2964c8cd07fcf6b933cbca25b455ee1
gyuezhang <47130297+gyuezhang@users.noreply.github.com>
2019-12-28 16:49:16 +0800

    log first

--- GRBM/PageLogMng/PageLogMngViewModel.cs
+++ GRBM/PageLogMng/PageLogMngViewModel.cs
@@ -1,4 +1,8 @@
-﻿using Stylet;
+﻿using GRModel;
+using GRSocket;
+using GRUtil;
+using Stylet;
+using System.Collections.Generic;
 
 namespace GRBM
 {
@@ -7,8 +11,46 @@ namespace GRBM
         public PageLogMngViewModel(WndMainViewModel _wndMainVM)
         {
             wndMainVM = _wndMainVM;
+
+            GRSocketHandler.getLogs += GRSocketHandler_getLogs;
+
+            GRSocketAPI.GetLogs();
         }
+
         private WndMainViewModel wndMainVM { get; set; }
 
+        #region SocketHandler
+
+        private void GRSocketHandler_getLogs(RES_STATE state, List<C_Log> logs)
+        {
+            switch (state)
+            {
+                case RES_STATE.OK:
+                    logLstBd = logs;
+                    break;
+                case RES_STATE.FAILED:
+                    wndMainVM.messageQueueBd.Enqueue("获取日志失败");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        #endregion SocketHandler
+
+        #region Bindings        
+
+        public List<C_Log> logLstBd { get; set; } = new List<C_Log>();
+        
+        #endregion Bindings
+
+        #region Actions
+
+        public void RefreshCmd()
+        {
+            GRSocketAPI.GetLogs();
+        }
+
+        #endregion Actions
     }
 }
