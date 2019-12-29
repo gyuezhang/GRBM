d12bd0244ab0f52fced1f0219b78788913f1ecd7
gyuezhang <47130297+gyuezhang@users.noreply.github.com>
2019-12-29 13:21:03 +0800

    init page bar

---
+++ GRBM/CtrlPageBar/CtrlPageBarViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Stylet;
+
+namespace GRBM
+{
+    class CtrlPageBarViewModel : Screen
+    {
+        public CtrlPageBarViewModel(WndMainViewModel _wndMainVM)
+        {
+            wndMainVM = _wndMainVM;
+        }
+        private WndMainViewModel wndMainVM { get; set; }
+
+    }
+}
